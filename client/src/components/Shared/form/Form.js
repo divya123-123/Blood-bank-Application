e71ae2cd4a0772f760860eb76110484983b8aadf
Divya Kala <121877809+divya123-123@users.noreply.github.com>
2024-02-17 13:27:18 +0530

    Add files via upload

---
+++ client/src/components/Shared/form/Form.js
@@ -0,0 +1,222 @@
+import React, { useState } from "react";
+import InputType from "./InputType";
+import { Link } from "react-router-dom";
+import { handleLogin, handleRegister } from "../../../services/authService";
+
+const Form = ({ formType, submitBtn, formTitle }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("donar");
+  const [name, setName] = useState("");
+  const [organizationName, setOrganizationName] = useState("");
+  const [hospitalName, setHospitalName] = useState("");
+  const [website, setWebsite] = useState("");
+  const [address, setAddress] = useState("");
+  const [phone, setPhone] = useState("");
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          if (formType === "login")
+            return handleLogin(e, email, password, role);
+          else if (formType === "register")
+            return handleRegister(
+              e,
+              name,
+              role,
+              email,
+              password,
+              organizationName,
+              hospitalName,
+              website,
+              address,
+              phone
+            );
+        }}
+      >
+        <h1 className="text-center">{formTitle}</h1>
+        <hr />
+        <div className="d-flex mb-3">
+          <div className="form-check ms-2">
+            <input
+              type="radio"
+              className="form-check-input"
+              name="role"
+              id="donarRadio"
+              value={"donar"}
+              onChange={(e) => setRole(e.target.value)}
+              defaultChecked
+            />
+            <label htmlFor="donarRadio" className="form-check-label">
+              Donar
+            </label>
+          </div>
+          <div className="form-check ms-2">
+            <input
+              type="radio"
+              className="form-check-input"
+              name="role"
+              id="adminRadio"
+              value={"admin"}
+              onChange={(e) => setRole(e.target.value)}
+            />
+            <label htmlFor="adminRadio" className="form-check-label">
+              Admin
+            </label>
+          </div>
+
+          <div className="form-check ms-2">
+            <input
+              type="radio"
+              className="form-check-input"
+              name="role"
+              id="hospitalRadio"
+              value={"hospital"}
+              onChange={(e) => setRole(e.target.value)}
+            />
+            <label htmlFor="hospitalRadio" className="form-check-label">
+              Hospital
+            </label>
+          </div>
+          <div className="form-check ms-2">
+            <input
+              type="radio"
+              className="form-check-input"
+              name="role"
+              id="organization"
+              value={"organization"}
+              onChange={(e) => setRole(e.target.value)}
+            />
+            <label htmlFor="organizationRadio" className="form-check-label">
+              Organization
+            </label>
+          </div>
+        </div>
+        {/*switch statement*/}
+        {(() => {
+          //eslint-disable-next-line
+          switch (true) {
+            case formType === "login": {
+              return (
+                <>
+                  <InputType
+                    labelText={"email"}
+                    labelfor={"forEmail"}
+                    inputType={"email"}
+                    name={"email"}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <InputType
+                    labelText={"password"}
+                    labelfor={"forPassword"}
+                    inputType={"password"}
+                    name={"password"}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </>
+              );
+            }
+            case formType === "register": {
+              return (
+                <>
+                  {(role === "admin" || role === "donar") && (
+                    <InputType
+                      labelText={"Name"}
+                      labelfor={"forName"}
+                      inputType={"text"}
+                      name={"name"}
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  )}
+                  {role === "organization" && (
+                    <InputType
+                      labelText={"Organization Name"}
+                      labelfor={"forOrganizationName"}
+                      inputType={"text"}
+                      name={"organizationName"}
+                      value={organizationName}
+                      onChange={(e) => setOrganizationName(e.target.value)}
+                    />
+                  )}
+                  {role === "hospital" && (
+                    <InputType
+                      labelText={"Hospital Name"}
+                      labelfor={"forHospitalName"}
+                      inputType={"text"}
+                      name={"hospitalName"}
+                      value={hospitalName}
+                      onChange={(e) => setHospitalName(e.target.value)}
+                    />
+                  )}
+
+                  <InputType
+                    labelText={"email"}
+                    labelfor={"forEmail"}
+                    inputType={"email"}
+                    name={"email"}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <InputType
+                    labelText={"password"}
+                    labelfor={"forPassword"}
+                    inputType={"password"}
+                    name={"password"}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <InputType
+                    labelText={"Website"}
+                    labelfor={"forWebsite"}
+                    inputType={"text"}
+                    name={"website"}
+                    value={website}
+                    onChange={(e) => setWebsite(e.target.value)}
+                  />
+                  <InputType
+                    labelText={"Address"}
+                    labelfor={"forAddress"}
+                    inputType={"text"}
+                    name={"address"}
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                  />
+                  <InputType
+                    labelText={"Phone"}
+                    labelfor={"forPhone"}
+                    inputType={"text"}
+                    name={"phone"}
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
+                </>
+              );
+            }
+          }
+        })()}
+
+        <div className="d-flex flex-row justify-content-between">
+          {formType === "login" ? (
+            <p>
+              Not registered yet ? Register
+              <Link to="/register"> Here !</Link>
+            </p>
+          ) : (
+            <p>
+              Already Registered
+              <Link to="/login"> Please Login !</Link>
+            </p>
+          )}
+          <button className="btn btn-primary" type="submit">
+            {submitBtn}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const cors = require("cors");
+const connectDB = require("./config/db");
+//dotenv config
+dotenv.config();
+
+//mongodb connection
+connectDB();
+//rest object
+const app = express();
+//middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+//routes
+app.use("/api/v1/test", require("./routes/testRoutes"));
+app.use("/api/v1/auth", require("./routes/authRoutes"));
+app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
+
+//port
+const PORT = process.env.PORT || 4000;
+
+//listen
+
+app.listen(PORT, () => {
+  console.log(
+    `Server Running in ${process.env.DEV_MODE} Modeon port ${process.env.PORT}`
+      .bgBlue.white
+  );
+});
